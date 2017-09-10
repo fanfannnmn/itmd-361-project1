06f97e6172a2addb89619c75e54da34f2bd8910f
Yifan Yao <fanfannnmn@users.noreply.github.com>
2017-09-10 12:01:29 -0500

    Script update

--- check.sh
+++ check.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
 # Check by HTML5 validator
+date
 vnu *.html
+vnu */*.html
+ls *.html
+ls */*.html
+echo "HTML5 validator runs successfully"
 
 # Check by Linter configurations for Stolley's classes (ITMD 361, COM 330/530)
 htmllint
+echo "htmllint runs successfully"
--- initialize/firstime_initialize.sh
+++ initialize/firstime_initialize.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# Please place this script under your project folder
 # Install nodejs at https://nodejs.org/en/download/
 # Install Git at https://git-scm.com/downloads
 
@@ -11,7 +12,8 @@ npm install -g htmllint-cli
 git init
 htmllint init
 
-# Linter configurations for Stolley's classes (ITMD 361, COM 330/530) 
+# Linter configurations for Stolley's classes (ITMD 361, COM 330/530)
 # https://gist.github.com/559aac5112928c7c24c628c6305b70b8
 rm .htmllintrc
 wget https://gist.github.com/profstolley/559aac5112928c7c24c628c6305b70b8/raw/f2c974bc495af93d86ebf0e93831e469ef218ba8/.eslintrc.json https://gist.github.com/profstolley/559aac5112928c7c24c628c6305b70b8/raw/f2c974bc495af93d86ebf0e93831e469ef218ba8/.eslintrc.json.with-comments https://gist.github.com/profstolley/559aac5112928c7c24c628c6305b70b8/raw/f2c974bc495af93d86ebf0e93831e469ef218ba8/.htmllintrc https://gist.github.com/profstolley/559aac5112928c7c24c628c6305b70b8/raw/f2c974bc495af93d86ebf0e93831e469ef218ba8/.stylelintrc
+ls -hl
