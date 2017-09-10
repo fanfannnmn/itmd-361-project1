b7d8f2e40ecf0028958063fbd262699269fbbe96
Yifan Yao <fanfannnmn@users.noreply.github.com>
2017-09-10 11:00:13 -0500

    Replace git clone to wget

--- init/init.sh
+++ init/init.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-# Please install nodejs first at https://nodejs.org/en/download/
+# Install nodejs at https://nodejs.org/en/download/
+# Install Git at https://git-scm.com/downloads
 
 # Install necessary packages
 npm install vnu-jar
@@ -11,11 +12,6 @@ git init
 htmllint init
 
 # Linter configurations for Stolley's classes (ITMD 361, COM 330/530) 
+# https://gist.github.com/559aac5112928c7c24c628c6305b70b8
 rm .htmllintrc
-git clone https://gist.github.com/559aac5112928c7c24c628c6305b70b8.git
-cd 559aac5112928c7c24c628c6305b70b8
-cp .eslintrc.json ../
-cp .eslintrc.json.with-comments ../
-cp .stylelintrc ../
-cp .htmllintrc ../
-cd ..
+wget https://gist.github.com/profstolley/559aac5112928c7c24c628c6305b70b8/raw/f2c974bc495af93d86ebf0e93831e469ef218ba8/.eslintrc.json https://gist.github.com/profstolley/559aac5112928c7c24c628c6305b70b8/raw/f2c974bc495af93d86ebf0e93831e469ef218ba8/.eslintrc.json.with-comments https://gist.github.com/profstolley/559aac5112928c7c24c628c6305b70b8/raw/f2c974bc495af93d86ebf0e93831e469ef218ba8/.htmllintrc https://gist.github.com/profstolley/559aac5112928c7c24c628c6305b70b8/raw/f2c974bc495af93d86ebf0e93831e469ef218ba8/.stylelintrc
